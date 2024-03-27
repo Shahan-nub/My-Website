752ddb0911330e1b0f0d2dc879174322348804e0
Shahan-nub <shahananwer786@gmail.com>
2024-03-28 02:33:00 +0530

    lights position on ipads

--- src/components/ui/MacOSlights.jsx
+++ src/components/ui/MacOSlights.jsx
@@ -3,7 +3,7 @@ import { GoDotFill } from "react-icons/go";
 export default function MacOSlights() {
   return (
     <div className="w-full hidden md:inline-block border-t rounded-t-xl h-[10%] bg-gradient-to-r from-[#3b3b3b] via-[#b2afa3] to-[#3b3b3b] ">
-          <div className=" flex gap-[6px] pl-4 pt-[5px] items-center">
+          <div className=" flex gap-[6px] pl-4 sm:pt-[3%] lg:pt-[1%] xl:pt-[5px] 2xl:pt-[10px] items-center">
             <div className="relative flex">
               <GoDotFill className="text-[#c05848]  rounded-full text-2xl" />
               <div className="absolute left-1/3 top-2 text-transparent shadow-[0_0_20px_4px_#c05848] rounded-full leading-[3px]">
