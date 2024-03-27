902994f7046107de4b17f0c16c9e9659428019c9
Shahan-nub <shahananwer786@gmail.com>
2024-03-27 14:38:20 +0530

    key to skillsmap

--- src/components/Skills/SkillsMap.jsx
+++ src/components/Skills/SkillsMap.jsx
@@ -6,7 +6,7 @@ export default function SkillsMap() {
     <div className='w-full flex justify-center md:flex-row flex-col gap-8 lg:gap-12'>
       {SkillsARR.map((SkillObj) => {
         return (
-            <SkillCard skillObj={SkillObj}></SkillCard>
+            <SkillCard key={skillObj.skillClass} skillObj={SkillObj}></SkillCard>
         )
       })}
     </div>
