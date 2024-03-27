fb025a3558746c1d5730db6a26a9f5b83af5ddcf
Shahan-nub <shahananwer786@gmail.com>
2024-03-28 02:42:06 +0530

    footer m p

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,7 +6,7 @@ import SkillsTitle from "./SkillsTitle";
 
 export default function Skills() {
   return (
-    <div className="flex flex-col my-5 lg:my-7 lg:w-[80vw] w-[90vw] items-center">
+    <div className="flex flex-col my-8 mb-16 lg:my-12 lg:w-[80vw] w-[90vw] items-center">
       <SkillsTitle></SkillsTitle>
       <SkillsMap></SkillsMap>
     </div>
